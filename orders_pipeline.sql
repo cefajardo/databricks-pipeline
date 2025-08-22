bb4841d38ce5a5a511e8c1b0d4016d5d8de1ca39
cefajardo <cefajardo>
2025-08-22 17:05:55 +0000

    Add status pipeline

--- orders_pipeline.sql
+++ orders_pipeline.sql
@@ -1,10 +1,15 @@
 -- A. Create the bronze streaming table
 
 CREATE OR REFRESH STREAMING TABLE 1_bronze_db.orders_bronze
+    COMMENT "Ingest order CSV files from cloud storage"
+    TBLPROPERTIES (
+        "quality" = "bronze"
+        --,"pipelines.reset.allowed" = false
+    )
 AS
 SELECT
     *,
-    current_timestamp AS processing_time,
+    current_timestamp() AS processing_time,
     _metadata.file_name AS source_file
 FROM STREAM read_files(
     "${source}/orders",
@@ -16,11 +21,13 @@ FROM STREAM read_files(
 -- Be carefull with FROM STREAM keyword
 
 CREATE OR REFRESH STREAMING TABLE 2_silver_db.orders_silver
-(
-    CONSTRAINT valid_notification EXPECT (notifications IN ('Y', 'x')),
-    CONSTRAINT valid_date EXPECT (order_timestamp > '2021-12-25') ON VIOLATION DROP ROW,
-    CONSTRAINT valid_customer_id EXPECT (customer_id IS NOT NULL) ON VIOLATION FAIL UPDATE
-)
+    (
+        CONSTRAINT valid_notification EXPECT (notifications IN ('Y', 'x')),
+        CONSTRAINT valid_date EXPECT (order_timestamp > '2021-12-25') ON VIOLATION DROP ROW,
+        CONSTRAINT valid_customer_id EXPECT (customer_id IS NOT NULL) ON VIOLATION FAIL UPDATE
+    )
+    COMMENT "Silver clean order data"
+    TBLPROPERTIES ( "quality" = "silver" )
 AS
 SELECT
     order_id,
@@ -32,6 +39,8 @@ FROM STREAM 1_bronze_db.orders_bronze;
 -- C. Create the materialized view aggregation from the orders_silver table
 
 CREATE OR REFRESH MATERIALIZED VIEW 3_gold_db.gold_orders_by_date
+    COMMENT "Aggregate order gold data for downstream analysis"
+    TBLPROPERTIES ( "quality" = "gold" )
 AS
 SELECT
     date(order_timestamp) AS order_date,
